0e2ef303a8d1f579f294330d50d770ba3d15780c
frank-job <jobf0837@gmail.com>
2026-09-18 13:42:56 +0300

    passcode

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const ensureLoggedIn = (req, res, next) => {
 
 router.get('/', (req, res) => {
     if (req.isAuthenticated && req.isAuthenticated()) {
-        return res.send(`Hello world, ${req.user.username || 'GitHub user'}! You are logged in. <a href="/logout">Logout</a>`);
+        return res.send(`Hello world, ${req.user.username || 'GitHub user'}! You are logged in. <a href="/logout">Logout</a> | <a href="/api-docs">Swagger</a>`);
     }
 
     return res.send(`Hello world. Please log in with GitHub: <a href="/login">/login</a>`);
--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/auth/github/callback',
     passport.authenticate('github', { failureRedirect: '/' }),
     (req, res) => {
         req.session.user = req.user;
-        res.redirect('/');
+        res.redirect('/api-docs');
     }
 );
 
